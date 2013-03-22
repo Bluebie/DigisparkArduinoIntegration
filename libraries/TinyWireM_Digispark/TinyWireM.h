2fadeddeda802681c3a32678c1518d4f28bd9185
Bluebie <a@creativepony.com>
2013-03-22 15:22:32 +1100

    TinyWireM: More minor style fixups

--- libraries/TinyWireM_Digispark/TinyWireM.h
+++ libraries/TinyWireM_Digispark/TinyWireM.h
@@ -11,22 +11,22 @@
  
  USAGE is modeled after the standard Wire library . . .
   Put in setup():
-	TinyWireM.begin(){                               // initialize I2C lib
+  TinyWireM.begin(){                               // initialize I2C lib
   To Send:
-	TinyWireM.beginTransmission(uint8_t slaveAddr){  // setup slave's address (7 bit address - same as Wire)
-	TinyWireM.send(uint8_t data){                    // buffer up bytes to send - can be called multiple times
-	someByte = TinyWireM.endTransmission(){          // actually send the bytes in the buffer
-	                                                 // returns (optional) 0 = sucess or see USI_TWI_Master.h for error codes
+  TinyWireM.beginTransmission(uint8_t slaveAddr){  // setup slave's address (7 bit address - same as Wire)
+  TinyWireM.send(uint8_t data){                    // buffer up bytes to send - can be called multiple times
+  someByte = TinyWireM.endTransmission(){          // actually send the bytes in the buffer
+                                                   // returns (optional) 0 = sucess or see USI_TWI_Master.h for error codes
   To Receive:
-	someByte = TinyWireM.requestFrom(uint8_t slaveAddr, uint8_t numBytes){      // reads 'numBytes' from slave's address
-	                                                 // (usage optional) returns 0= success or see USI_TWI_Master.h for error codes
-	someByte = TinyWireM.receive(){                  // returns the next byte in the received buffer - called multiple times
-	someByte = TinyWireM.available(){                // returns the number of unread bytes in the received buffer
+  someByte = TinyWireM.requestFrom(uint8_t slaveAddr, uint8_t numBytes){      // reads 'numBytes' from slave's address
+                                                   // (usage optional) returns 0= success or see USI_TWI_Master.h for error codes
+  someByte = TinyWireM.receive(){                  // returns the next byte in the received buffer - called multiple times
+  someByte = TinyWireM.available(){                // returns the number of unread bytes in the received buffer
 
-	TODO:	(by others!)
-	- merge this class with TinyWireS for master & slave support in one library
+  TODO: (by others!)
+  - merge this class with TinyWireS for master & slave support in one library
 
-	This library is free software; you can redistribute it and/or modify it under the
+  This library is free software; you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
   Foundation; either version 2.1 of the License, or any later version.
   This program is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -45,14 +45,14 @@
 class USI_TWI
 {
   private:
-	static uint8_t USI_Buf[];           // holds I2C send and receive data
-	static uint8_t USI_BufIdx;          // current number of bytes in the send buff
-	static uint8_t USI_LastRead;        // number of bytes read so far
-	static uint8_t USI_BytesAvail;      // number of bytes requested but not read
-	
+    static uint8_t USI_Buf[];           // holds I2C send and receive data
+    static uint8_t USI_BufIdx;          // current number of bytes in the send buff
+    static uint8_t USI_LastRead;        // number of bytes read so far
+    static uint8_t USI_BytesAvail;      // number of bytes requested but not read
+  
   public:
- 	USI_TWI();
-	void begin();
+    USI_TWI();
+    void begin();
     void beginTransmission(uint8_t);
     void send(uint8_t);
     uint8_t endTransmission();
